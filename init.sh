7468b428680383c2b844fb7a2151f808f016c3d4
Émilien Schultz <emilien.schultz@gmail.com>
2024-04-23 12:49:59 +0200

    Add files via upload

---
+++ init.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Variables $1 &2 etc.
+
+# Exemple : https://git.drees.fr/drees_code_public/ressources/tutos/-/blob/diffusion/contenu/init.sh
+# On enregistre tous les logs dans log.out pour pouvoir déboguer
+exec 3>&1 4>&2
+trap 'exec 2>&4 1>&3' 0 1 2 3
+exec 1>log.out 2>&1
+
+# Pareil pour les variables d'environnement
+env | sort > env_init.out
+
+touch "test"
+
+# Cloner le dépot
+git clone https://github.com/emilienschultz/test.git
+python install -m test/requirements.txt
